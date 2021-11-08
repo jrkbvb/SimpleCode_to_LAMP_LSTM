aa1a3f4dba368b3b67c1b6e3c3cc0545d5149158
Dayne Howard <dayneh@mit.edu>
2021-11-08 11:28:40 -0500

    Added column for time vector

--- save_lstm_results.py
+++ save_lstm_results.py
@@ -8,29 +8,32 @@ def save_lstm_results(train_lstm_output, val_lstm_output, test_lstm_output, save
 	for i in save_data_args.train:
 		x = data_info_args.train_sc[i].rfind("/") + 1
 		filename = save_data_args.prefix + data_info_args.train_sc[i][x:] + ".txt"
-		save_data(filename, train_lstm_output[i,:,:], save_data_args)
+		time_vector = np.loadtxt(data_info_args.train_sc[i] + ".mot",skiprows=2)[:,0:1]
+		save_data(filename, train_lstm_output[i,:,:], save_data_args, time_vector)
 	for i in save_data_args.val:
 		x = data_info_args.train_sc[i].rfind("/") + 1
 		filename = save_data_args.prefix + data_info_args.val_sc[i][x:] + ".txt"
-		save_data(filename, val_lstm_output[i,:,:], save_data_args)
+		time_vector = np.loadtxt(data_info_args.val_sc[i] + ".mot",skiprows=2)[:,0:1]
+		save_data(filename, val_lstm_output[i,:,:], save_data_args, time_vector)
 	for i in save_data_args.test:
 		x = data_info_args.train_sc[i].rfind("/") + 1 
 		filename = save_data_args.prefix + data_info_args.test_sc[i][x:] + ".txt"
-		save_data(filename, test_lstm_output[i,:,:], save_data_args)
+		time_vector = np.loadtxt(data_info_args.test_sc[i] + ".mot",skiprows=2)[:,0:1]
+		save_data(filename, test_lstm_output[i,:,:], save_data_args, time_vector)
 
-def save_data(filename, output, save_data_args):
-	my_header = "Zcg        Roll        Pitch"
+def save_data(filename, output, save_data_args, time_vector):
+	my_header = "Time        Zcg        Roll        Pitch"
 	if len(save_data_args.output_path)>0 and not save_data_args.output_path[-1]=="/":
 		save_data_args.output_path += "/"
 		print("The user specified output_path did not have a / at the end, so I added one")
 	try:
-		np.savetxt(save_data_args.output_path + filename, output, fmt="%.5f" ,delimiter='     ', header=my_header)
+		np.savetxt(save_data_args.output_path + filename, np.concatenate((time_vector[:output.shape[0]], output),axis=1), fmt="%.5f" ,delimiter='     ', header=my_header)
 		print("Successfully saved output in ", save_data_args.output_path + filename)
 	except:
 		print("Error saving file (possibly the output path). Attempting to save in current directory")
 		print("output path is: ", save_data_args.output_path)
 		try:
-			np.savetxt(filename, output, delimiter='     ', fmt="%.5f", header=my_header)
+			np.savetxt(filename, np.concatenate((time_vector[:output.shape[0]], output),axis=1), delimiter='     ', fmt="%.5f", header=my_header)
 			print("Successfully saved output in current directory: ", filename)
 		except:
 			print("Failed to save file called ", filename)
