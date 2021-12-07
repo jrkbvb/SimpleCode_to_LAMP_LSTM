Fixed the saving data to files so that the data was unstandardized (back to original units) whereas before it was still standardized

plot_lstm_results.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from peak_errors_scatter_plot import peak_errors_scatter_plot
+from unstandardize import un_standardize
 import numpy as np
 def plot_lstm_results(train_target, val_target, test_target, train_lstm_output, val_lstm_output, test_lstm_output, plot_args, data_info_args, std_factors):
 	if plot_args.plotting_mode==False:
@@ -233,27 +234,6 @@ def maxima_plot(train_target, val_target, test_target, train_lstm_output, val_ls
 	axs3[0].axes.set_aspect('equal')
 	axs3[1].axes.set_aspect('equal')
 	axs3[2].axes.set_aspect('equal')
-
-
-def un_standardize(target, lstm_output, sc_filepath, std_factors):
-	realization_length = lstm_output.shape[0]
-	simple_data = np.loadtxt(sc_filepath + ".mot",skiprows=2)[:realization_length,[3,4,5]]
-
-	roll_mean = np.mean(simple_data[:,1])
-	roll_std = np.std(simple_data[:,1])
-	pitch_mean = np.mean(simple_data[:,2])
-	pitch_std = np.std(simple_data[:,2])
-
-	lamp_data = np.copy(target)
-	lstm_data = np.copy(lstm_output)
-	lamp_data[:,0] = target[:,0]*std_factors[3] + std_factors[2] #zcg
-	lamp_data[:,1] = target[:,1]*roll_std + roll_mean
-	lamp_data[:,2] = target[:,2]*pitch_std + pitch_mean
-	lstm_data[:,0] = lstm_output[:,0]*std_factors[3] + std_factors[2] #zcg
-	lstm_data[:,1] = lstm_output[:,1]*roll_std + roll_mean
-	lstm_data[:,2] = lstm_output[:,2]*pitch_std + pitch_mean
-
-	return simple_data, lamp_data, lstm_data
 	
 
 	

save_lstm_results.py
@@ -1,6 +1,7 @@
 import numpy as np
+from unstandardize import un_standardize_output
 
-def save_lstm_results(train_lstm_output, val_lstm_output, test_lstm_output, save_data_args, data_info_args):
+def save_lstm_results(train_lstm_output, val_lstm_output, test_lstm_output, save_data_args, data_info_args, std_factors):
 	'''Saves LSTM results to a text file.'''
 	if save_data_args.save_data_mode == False:
 		return
@@ -9,17 +10,20 @@ def save_lstm_results(train_lstm_output, val_lstm_output, test_lstm_output, save
 		x = data_info_args.train_sc[i].rfind("/") + 1
 		filename = save_data_args.prefix + data_info_args.train_sc[i][x:] + ".txt"
 		time_vector = np.loadtxt(data_info_args.train_sc[i] + ".mot",skiprows=2)[:,0:1]
-		save_data(filename, train_lstm_output[i,:,:], save_data_args, time_vector)
+		temp_lstm_output = un_standardize_output(train_lstm_output[i,:,:], data_info_args.train_sc[i], std_factors)
+		save_data(filename, temp_lstm_output, save_data_args, time_vector)
 	for i in save_data_args.val:
 		x = data_info_args.train_sc[i].rfind("/") + 1
 		filename = save_data_args.prefix + data_info_args.val_sc[i][x:] + ".txt"
 		time_vector = np.loadtxt(data_info_args.val_sc[i] + ".mot",skiprows=2)[:,0:1]
-		save_data(filename, val_lstm_output[i,:,:], save_data_args, time_vector)
+		temp_lstm_output = un_standardize_output(val_lstm_output[i,:,:], data_info_args.train_sc[i], std_factors)
+		save_data(filename, temp_lstm_output, save_data_args, time_vector)
 	for i in save_data_args.test:
 		x = data_info_args.train_sc[i].rfind("/") + 1 
 		filename = save_data_args.prefix + data_info_args.test_sc[i][x:] + ".txt"
 		time_vector = np.loadtxt(data_info_args.test_sc[i] + ".mot",skiprows=2)[:,0:1]
-		save_data(filename, test_lstm_output[i,:,:], save_data_args, time_vector)
+		temp_lstm_output = un_standardize_output(test_lstm_output[i,:,:], data_info_args.train_sc[i], std_factors)
+		save_data(filename, temp_lstm_output, save_data_args, time_vector)
 
 def save_data(filename, output, save_data_args, time_vector):
 	my_header = "Time        Zcg        Roll        Pitch"