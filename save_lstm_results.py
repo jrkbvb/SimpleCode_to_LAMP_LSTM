c92d40e0289c3dab8fc4b2914535b3a53dff50d1
Dayne Howard <dayneh@mit.edu>
2021-12-08 09:33:26 -0500

    fixed bug in saving output text files

--- save_lstm_results.py
+++ save_lstm_results.py
@@ -13,16 +13,16 @@ def save_lstm_results(train_lstm_output, val_lstm_output, test_lstm_output, save
 		temp_lstm_output = un_standardize_output(train_lstm_output[i,:,:], data_info_args.train_sc[i], std_factors)
 		save_data(filename, temp_lstm_output, save_data_args, time_vector)
 	for i in save_data_args.val:
-		x = data_info_args.train_sc[i].rfind("/") + 1
+		x = data_info_args.val_sc[i].rfind("/") + 1
 		filename = save_data_args.prefix + data_info_args.val_sc[i][x:] + ".txt"
 		time_vector = np.loadtxt(data_info_args.val_sc[i] + ".mot",skiprows=2)[:,0:1]
-		temp_lstm_output = un_standardize_output(val_lstm_output[i,:,:], data_info_args.train_sc[i], std_factors)
+		temp_lstm_output = un_standardize_output(val_lstm_output[i,:,:], data_info_args.val_sc[i], std_factors)
 		save_data(filename, temp_lstm_output, save_data_args, time_vector)
 	for i in save_data_args.test:
-		x = data_info_args.train_sc[i].rfind("/") + 1 
+		x = data_info_args.test_sc[i].rfind("/") + 1 
 		filename = save_data_args.prefix + data_info_args.test_sc[i][x:] + ".txt"
 		time_vector = np.loadtxt(data_info_args.test_sc[i] + ".mot",skiprows=2)[:,0:1]
-		temp_lstm_output = un_standardize_output(test_lstm_output[i,:,:], data_info_args.train_sc[i], std_factors)
+		temp_lstm_output = un_standardize_output(test_lstm_output[i,:,:], data_info_args.test_sc[i], std_factors)
 		save_data(filename, temp_lstm_output, save_data_args, time_vector)
 
 def save_data(filename, output, save_data_args, time_vector):
