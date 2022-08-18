8c347403f66b3cccaae36ed0f4637714790f6663
jrkbvb <jarodrkramer@gmail.com>
2022-08-18 13:17:44 -0400

    Update LSTM_batch.bat

--- LSTM_batch.bat
+++ LSTM_batch.bat
@@ -1,15 +1,15 @@
 cd "H:\OneDrive - Massachusetts Institute of Technology\Thesis\SimpleCode_to_LAMP_LSTM\SimpleCode_and_LAMP_generator"
 
-set VAR_1=wide_training.txt
-set VAR_2=wide_validation.txt
-set VAR_3=wide_test.txt
+::set VAR_1=wide_training.txt
+::set VAR_2=wide_validation.txt
+set VAR_3=par_test.txt
 
-echo %VAR_1% | python SimpleCodePy.py
-echo %VAR_2% | python SimpleCodePy.py
-echo %VAR_3% | python SimpleCodePy.py
+::echo %VAR_1% | python SimpleCodePy.py
+::echo %VAR_2% | python SimpleCodePy.py
+::echo %VAR_3% | python SimpleCodePy.py
 
-echo %VAR_1% | python LAMPPy.py
-echo %VAR_2% | python LAMPPy.py
+::echo %VAR_1% | python LAMPPy.py
+::echo %VAR_2% | python LAMPPy.py
 echo %VAR_3% | python LAMPPy.py
 
 :: cd "H:\OneDrive - Massachusetts Institute of Technology\Thesis\SimpleCode_to_LAMP_LSTM"
